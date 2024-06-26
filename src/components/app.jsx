ea07372338466cfff1a01edfadb979297d1e4433
Zhan Xiang Zheng <z@zhans-air.mynetworksettings.com>
2024-06-26 13:36:40 -0400

    Loading Screen

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,21 @@
 import './app.css';
+import { useEffect, useState } from "react";
 import Header from './header/header';
 import Landing from './landing/landing'
 import Timeline from './timeline/timeline'
 import Projects from './projects/projects';
 import Footer from './footer/footer'
+import Loading from "./loading/loading";
+
 
 export default function App() {
+  const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 3300)
+    }, [])
+    if (loading) {
+        return <Loading/>
+    }
   return (
     <div className="App">
       <Header />
