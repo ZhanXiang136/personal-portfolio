4709862b5ace5ba41350aede18587165810ec1ed
Zhan Xiang Zheng <zhanxiangzheng136@gmail.com>
2024-07-19 14:46:52 -0400

    project section done

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,37 +1,69 @@
-import './projects.css'
-import PokemonBattle from '../../assets/pokemon-battle-sample.gif'
-import PokemonMenu from '../../assets/pokemon-main.png'
-import CodeOverFlowMaze from '../../assets/code-overflow-maze.png'
-import CodeOverFlowMazeSample from '../../assets/code-overflow-maze-sample.gif'
+import "./projects.css";
+import PokemonBattle from "../../assets/pokemon-battle-sample.gif";
+import PokemonMenu from "../../assets/pokemon-main.png";
+import CodeOverFlowMaze from "../../assets/code-overflow-maze.png";
+import CodeOverFlowMazeSample from "../../assets/code-overflow-maze-sample.gif";
+import React from "react";
+import FadeComponent from "../fadeInOutComponent/fadeInOutComponent";
 
-export default function Projects() {
+const ProjectCard = ({
+  url,
+  backgroundImage,
+  mainText,
+  hoverImage,
+  hoverText,
+}) => {
   return (
-      <section className="projects" id='projects'>
-        <h2>Projects</h2>
-        <ul className="projects-list">
-          <li>
-            <a href="https://github.com/ZhanXiang136/PokemonGame" alt='Pokemon Battle'>
-              <img id='pokemon-image' src={PokemonMenu} alt='Pokemon Battle'
-              onMouseEnter={() => {document.querySelector("#pokemon-image").src = PokemonBattle}} 
-              onMouseLeave={() => {document.querySelector("#pokemon-image").src = PokemonMenu}}/>
-              <h3>Pokemon Battle Simulator</h3>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/dhuang6334/Code-Overflow" alt="Maze Runner">
-              <img id='maze' src={CodeOverFlowMaze} alt="Project 2" 
-              onMouseEnter={() => {document.querySelector("#maze").src = CodeOverFlowMazeSample}} 
-              onMouseLeave={() => {document.querySelector("#maze").src = CodeOverFlowMaze}}/>
-              <h3>Maze</h3>
-            </a>
-          </li>
-          <li>
-            <a href="https://zhanxiangzheng.me" alt='None'>
-              <img src="https://via.placeholder.com/300x200.png?text=Project+3" alt="Project 3" />
-              <h3>Project 3</h3>
-            </a>
-          </li>
-        </ul>
-      </section>
-  )
-}
+    <FadeComponent direction="down">
+      <div className="project-card">
+        <a href={url} alt={mainText}>
+          <img src={backgroundImage} alt="Project Background" />
+          <div className="overlay">
+            <div className="text-content">{mainText}</div>
+            <div className="hover-content">
+              <img src={hoverImage} alt="Hover Background" />
+              <div>{hoverText}</div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </FadeComponent>
+  );
+};
+
+const projects = [
+  {
+    backgroundImage: PokemonMenu,
+    mainText: "Pokemon Battle Simulator",
+    hoverImage: PokemonBattle,
+    hoverText: "Click Me",
+    url: "https://github.com/ZhanXiang136/PokemonGame",
+  },
+  {
+    backgroundImage: CodeOverFlowMaze,
+    mainText: "Infinite Maze Runner",
+    hoverImage: CodeOverFlowMazeSample,
+    hoverText: "Click Me",
+    url: "https://github.com/dhuang6334/Code-Overflow",
+  },
+  // Add more projects as needed
+];
+
+const Projects = () => {
+  return (
+    <div className="project-section">
+      {projects.map((project, index) => (
+        <ProjectCard
+          key={index}
+          url={project.url}
+          backgroundImage={project.backgroundImage}
+          mainText={project.mainText}
+          hoverImage={project.hoverImage}
+          hoverText={project.hoverText}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Projects;
